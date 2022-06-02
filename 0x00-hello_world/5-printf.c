2d513b0442f30e00f8dce37cf87a9bd5357a53b3
GeraniumR <rozanowino@gmail.com>
2022-06-02 15:20:10 -0700

    5printfc

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ *  * main - my entry point
+ *
+ *   * Description: print with proper grammar, but the outcome is a piece of art,
+ *
+ *    * Return: 0
+ *
+ *     */
+
+int main(void)
+
+{
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+
+}

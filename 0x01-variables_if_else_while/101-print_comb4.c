8930ba822090ffa7579725d38ea1331d8dce12a4
GeraniumR <rozanowino@gmail.com>
2022-06-07 17:57:16 -0700

    print comb

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -3,18 +3,15 @@
  * main - my entry point
  * Return: (0) - success
  */
-
 int main(void)
 {
 	int i, j, z;
-
 	for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
 		{
 		for (z = 0; z <= 9; z++)
 		{
-
 			if (i != j && j != z && i != z && i < j && z > j)
 			{
 				putchar(i + '0');

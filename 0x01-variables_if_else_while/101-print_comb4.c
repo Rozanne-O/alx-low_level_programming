1bbbd227136e6f8e6af3a02cddcf717c88285873
GeraniumR <rozanowino@gmail.com>
2022-06-07 17:41:02 -0700

    print comb

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ * main - my entry point
+ * Return: (0) - success
+ */
+
+int main(void)
+{
+	int i, j, z;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (z = 0; z <= 9; z++)
+		{
+			if (i != j && j != z && i != z && i < j && z > j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(z + '0');
+				if (!(i == 7 && j == 8 && z == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

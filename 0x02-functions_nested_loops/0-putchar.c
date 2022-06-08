e14801c1976277b6b98dc955bd5fb497a73987f0
GeraniumR <rozanowino@gmail.com>
2022-06-07 19:02:59 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * main - Entry point
- *
+ * main - my entry point
+ * Description: program that prints _putchar
  * Return: 0
  */
-
 int main(void)
 {
-	char x[] = "_putchar";
-	int i;
-
-	for (i = 0; i < 8; i++)
-	{
-		_putchar(x[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }

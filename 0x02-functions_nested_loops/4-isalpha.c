101d139159e58d0e0061aa4ff160cd19d4ee5ccd
GeraniumR <rozanowino@gmail.com>
2022-06-07 19:55:27 -0700

    isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: 0
+ * _isalpha - checkes is c is a letter
+ * @c: character being checked
+ * Return: 1 if c is a letter and 0 if otherwise
  */
-
-int main(void)
+int _isalpha(char c)
+{
+	if (c < 65 || c > 122)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
 
